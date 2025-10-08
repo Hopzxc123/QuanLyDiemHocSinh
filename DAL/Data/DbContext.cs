c0c52fae0ccbc93e92305b424b0def23aa0aa304
Kendy205 <thanhtung002zzz@gmail.com>
2025-10-08 08:11:51 +0700

    add frmqlthongtinsinhvien

---
+++ DAL/Data/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDiemTruongCap3.DAL.Data
+{
+    public class DbContext : IDisposable
+    {
+        private readonly SqlConnection _connection;
+
+        public SqlConnection Connection => _connection;
+        public DbContext(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+        public void Dispose()
+        {
+            if (_connection != null && _connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+        }
+    }
+}
---
+++ GUI/frmQuanLiThongTinSinhVien.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmQuanLiThongTinSinhVien : Form
+    {
+        public frmQuanLiThongTinSinhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmQuanLiThongTinSinhVien_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
