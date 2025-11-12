e530b41ff4976e69006cf9ac993f7684206da088
unknown <damhien22042005@gmail.com>
2025-11-12 20:08:47 +0700

    fix lai giao vien

--- GUI/QuanLyGiaoVien.Designer.cs
+++ GUI/QuanLyGiaoVien.Designer.cs
@@ -242,15 +242,18 @@
             this.cboChuyenMon.IntegralHeight = false;
             this.cboChuyenMon.ItemHeight = 20;
             this.cboChuyenMon.Items.AddRange(new object[] {
-            "Toán ",
-            "Vật Lý",
-            "Hóa Học",
-            "Sinh Học",
-            "Ngữ Văn",
-            "Lịch Sử",
-            "Địa Lý",
+            "Công nghệ",
+            "Địa lý",
+            "Giáo dục công dân",
+            "Thể chất",
+            "Hóa học",
+            "Lịch sử",
+            "Ngữ văn",
+            "Sinh học",
             "Tiếng Anh",
-            "GDCD"});
+            "Tin học",
+            "Toán",
+            "Vật lý"});
             this.cboChuyenMon.Location = new System.Drawing.Point(151, 353);
             this.cboChuyenMon.Name = "cboChuyenMon";
             this.cboChuyenMon.Size = new System.Drawing.Size(356, 26);
@@ -632,6 +635,7 @@
             this.DataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.DataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             this.DataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            this.DataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
             // lblTTGV
             // 
